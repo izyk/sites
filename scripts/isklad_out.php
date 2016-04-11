65f788f2924abdccd6996fe04e02e914a1d1b2d9
Ilya <github@izyk.ru>
2016-04-11 21:02:36 +0300

    Start scripting

---
+++ scripts/isklad_out.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<base href="http://isklad.ru/">
+    <title><?php echo $page_title; ?></title>
+    <meta name="description" content="<?php echo $page_desc; ?>">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/default.css"/>
+    <link rel="stylesheet" href="css/main.css"/>
+</head>
+
+<body>
+    
+<header>
+    <div class="inner clearfix">
+        
+        <a href="./"><img src="images/2_logo.png" alt="" class="logo"></a>
+
+        <div class="contact-info">
+            <div class="title">Надежный склад</div>
+            <p>+7 (495) 123-45-67</p>
+            <p><a href="mailto:info@isklad.ru">info@isklad.ru</a></p>
+        </div>
+
+    </div>
+</header>
+
+<nav id="main-nav">
+    <div class="inner clearfix">
+        <a href="/" class="menu-item <?php echo $menu_active[0]; ?>"><i class="fa fa-home"></i> У нас</a>
+        <a href="youcan/" class="menu-item <?php echo $menu_active[1]; ?>"><i class="fa fa-user"></i> Вы можете</a>
+        <a href="keep/" class="menu-item <?php echo $menu_active[2]; ?>"><i class="fa fa-lock"></i> Сохранить</a>
+        <a href="goods/" class="menu-item <?php echo $menu_active[3]; ?>"><i class="fa fa-archive"></i> Свой груз</a>
+    </div>
+</nav>
+
+<section id="content">
+    <div class="inner"><?php echo $page_body; ?>
+    </div>
+</section>
+
+<nav id="free">
+    <div class="inner">
+        <a href="/tariff/" class="menu-item <?php echo $menu_active[4]; ?>"><i class="fa fa-thumbs-up"></i> Бесплатно*.</a>
+        <div class="desc">*Два дня с момента разгрузки.</div>
+    </div>
+</nav>
+
+<footer>
+    <div class="inner">
+        <nav id="footer-nav" class="clearfix">
+            <a href="#">Услуги</a>
+            <a href="#">Заявки</a>
+            <a href="#">О нас</a>
+            <a href="#">О нас еще раз</a>
+            <a href="#">Цены</a>
+            <a href="#">Ссылки</a>
+        </nav>
+    </div>
+</footer>
+
+</body>
+
+</html>
