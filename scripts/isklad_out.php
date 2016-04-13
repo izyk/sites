1c32f5f11e4d50d14e308176fdf11c08ba03ab11
Ilya <github@izyk.ru>
2016-04-13 14:35:13 +0300

    Keep

--- scripts/isklad_out.php
+++ scripts/isklad_out.php
@@ -4,13 +4,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title><?php echo $page_title; ?></title>
     <meta name="description" content="<?php echo $page_desc; ?>">
-	<meta name="keywords" content="склад, ответственное хранение, складские услуги, сервис-ориентированный склад">
+	<meta name="keywords"  content="<?php echo $page_kwords; ?>">
 	<meta name="author" content="Ilia Zykov">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/default.css"/>
     <link rel="stylesheet" href="/css/main.css"/>
+    <link rel="stylesheet" href="<?php echo $page_css; ?>"/>
 </head>
 
 <body>
@@ -22,7 +23,7 @@
 
         <div class="contact-info">
             <div class="title">Надежный склад</div>
-            <p>+7 (495) 123-45-67</p>
+            <p>+7 (977) 948-55-12</p>
             <p><a href="mailto:info@isklad.ru">info@isklad.ru</a></p>
         </div>
 
@@ -32,8 +33,8 @@
 <nav id="main-nav">
     <div class="inner clearfix">
         <a href="/" class="menu-item <?php echo $menu_active[0]; ?>"><i class="fa fa-home"></i> У нас</a>
-        <a href="/youcan/" class="menu-item <?php echo $menu_active[1]; ?>"><i class="fa fa-user"></i> Вы можете</a>
-        <a href="/keep/" class="menu-item <?php echo $menu_active[2]; ?>"><i class="fa fa-lock"></i> Сохранить</a>
+        <a href="/services/" class="menu-item <?php echo $menu_active[1]; ?>"><i class="fa fa-user"></i> Вы можете</a>
+        <a href="/orders/" class="menu-item <?php echo $menu_active[2]; ?>"><i class="fa fa-lock"></i> Сохранить</a>
         <a href="/goods/" class="menu-item <?php echo $menu_active[3]; ?>"><i class="fa fa-archive"></i> Свой груз</a>
     </div>
 </nav>
@@ -53,11 +54,11 @@
 <footer>
     <div class="inner">
         <nav id="footer-nav" class="clearfix">
-            <a href="/">О нас</a>
-            <a href="/youcan/">Услуги</a>
-            <a href="/keep/">Клиентам</a>
-            <a href="/goods/">Товары</a>
-            <a href="/tariff/">Цены</a>
+            <a href="/">Контакты</a>
+            <a href="/services/">Услуги</a>
+            <a href="/orders/">Заказы</a>
+            <a href="/goods/">Ограничения</a>
+            <a href="/tariff/">Стоимость</a>
         </nav>
     </div>
 </footer>
